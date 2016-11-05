f4938438976e1cd4e33c7b52019660d2ecce59e4
emilMircea <emil.mircea30@gmail.com>
2016-11-05 14:23:13 +0100

    added the photo page which imports the Photo component

---
+++ pages/photo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Photo from '../components/frame'
+import { style } from 'next/css'
+
+export default ( { url: { query: {id} } } ) => (
+  <div className={style(styles.permalink)}>
+    <div className={style(styles.wrap)}>
+      <Photo id={id} />
+    </div>
+  </div>
+)
+
+const styles = {
+  permalink: {
+    padding: '100px',
+    textAlign: 'center'
+  },
+
+  wrap: {
+    display: 'inline-block',
+    border: '1px solid #999',
+    margin: 'auto'
+  }
+}
