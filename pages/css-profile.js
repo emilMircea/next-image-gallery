a5bf69e14d48682d1b3503bb1f9199344bc0cebf
emilMircea <emil.mircea30@gmail.com>
2016-11-05 14:17:00 +0100

    added css-profile for h1 title and username

---
+++ pages/css-profile.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { style } from 'next/css'
+
+export default ( { url: { query: { id } } } ) => (
+  <div className={style{styles.main}}>
+    <h1 className={style{styles.heading}}>
+      User Profile:
+      { ' ' }
+      <b className={style(styles.username)}>{id}</b>
+    </h1>
+
+  </div>
+)
+
+const styles = {
+  main: {
+    padding: '100px'
+  },
+
+  heading: {
+    font: '15px Monaco'
+  },
+
+  username: {
+    color: 'blue'
+  }
+}
